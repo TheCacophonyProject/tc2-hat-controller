33645a966d12e45cbbbc630ea61e866f9f7dc4c9
Alan McNatty <alan@encounter.nz>
2025-06-13 10:41:33 +1200

    Two part commit 1. Changed the lastFrame number check for whether or not it was the same object to a time based 'cool-down' if the same object (What) is predicted within the last minute assume it's the same object
    2.  Basically cleanup white-space and a few repeated if's to if elses

--- cmd/tc2-hat-comms/at-esl.go
+++ cmd/tc2-hat-comms/at-esl.go
@@ -17,8 +17,8 @@ type ATESLMessenger struct {
 }
 
 type ATESLLastPrediction struct {
-	Frame int32
 	What string
+	When time.Time
 }
 
 func processATESL(config *CommsConfig, testClassification *TestClassification, eventChannel chan event) error {
@@ -86,38 +86,39 @@ func (a ATESLMessenger) processTrackingEvent(t trackingEvent, l *ATESLLastPredic
 	log.Debugf("Received new tracking event What: %v, Confidence : %v, Region: %v, LastPredictionFrame: %v, Frame: %v",
                     t.What, t.Confidence, t.Region, t.LastPredictionFrame, t.Frame)
 
-	if t.Frame == 1 {
-		l.Frame = 0 // let's start again
-		l.What = ""
-	}
-
-	// We can do without false-positives :)
-	if t.What == "false-positive" {
-		return nil
-	}
-
-	// What:possum Confidence:99 Region:[16 65 29 79] Frame:218 Mass:17 BlankRegion:false Tracking:true LastPredictionFrame:194
-	// We don't really need tracks - just process predictions
 	if t.Frame != t.LastPredictionFrame {
 		return nil
 	}
 
-	// It's a prediction frame but within the same video stream and prediction so skip until a new stream starts
-	if l.Frame > 0 && t.Frame > l.Frame && t.What == l.What {
-		log.Infof("Skipping tracking prediction (frame) event: saved (%v) v event (%v) frame [%v v %v ]",
-				l.Frame, t.LastPredictionFrame, l.What, t.What)
+	var coolDownSeconds float64 = 60.0
+	lastPrediction := time.Since(l.When).Seconds()
+
+	// It's a prediction frame, but it's the same object within the last 60 seconds - let's skip notifying (likely same object)
+	if t.What == l.What && lastPrediction < coolDownSeconds {
+		log.Infof("Skipping prediction of %v - same as last prediction and within %vs", l.What, lastPrediction)
+
+		// But .. let's update the time we last saw it to now - if it's just sitting in a trap or in front of us
+		// we don't need to keep notifying
+		l.When = time.Now()
 		return nil
 	}
 
 	log.Infof("Processing tracking prediction (frame) event What: %v, Confidence : %v, Region: %v, Frame: %v",
                     t.What, t.Confidence, t.Region, t.Frame)
 
-	targetConfidence := int32(0)
+	var targetConfidence int32 = 0
 	target := false
 	// We've found an object - is it a target (trapable) species?
 	if _, found := a.trapSpecies["any"]; found {
+
+		// We can do without false-positives, not quite any :)
+		if t.What == "false-positive" {
+			return nil
+		}
+
 		target = true
 		targetConfidence = a.trapSpecies["any"]
+
 	} else if _, found := a.trapSpecies[t.What]; found {
 		target = true
 		targetConfidence = a.trapSpecies[t.What]
@@ -127,7 +128,8 @@ func (a ATESLMessenger) processTrackingEvent(t trackingEvent, l *ATESLLastPredic
 		log.Infof("Track prediction of a target species with confidence: %s,%d", t.What, t.Confidence)
 
 		atCmd := fmt.Sprintf("AT+CAM=%s,%d", t.What, t.Confidence)
-		l.Frame = t.Frame // remember the frame
+		l.What = t.What   	// Remember the object
+		l.When = time.Now()	// Remember when we detected it
 
 		err := sendATCommand(atCmd, a.baudRate)
 		if err != nil {
@@ -144,46 +146,40 @@ func sendATWakeUp(baudRate int) error {
 	log.Debugf("Wake up serial device.")
 	payload := []byte("\r\rAT\r")
 
-	retries := 5 // somewhat random - but don't hold it open forever if nothing is coming back
+	retries := 5 // somewhat random - but don't hold up the process if ther serial receiver is dead
 	attempt := 1
 
-    for {
-    	log.Infof("Sending AT wakeup command[%d]: %q", attempt, string(payload))
+	for {
+		log.Infof("Sending AT wakeup command[%d]: %q", attempt, string(payload))
 
-    	response, err := serialhelper.SerialSendReceive(1, gpio.High, gpio.Low, 10*time.Second, payload, baudRate)
-    	if err != nil {
-    		return fmt.Errorf("serial send receive error: %w", err)
-    	}
-	log.Debugf("Raw AT response: %q, %v", string(response), response)
+		response, err := serialhelper.SerialSendReceive(1, gpio.High, gpio.Low, 10*time.Second, payload, baudRate)
+		if err != nil {
+			return fmt.Errorf("serial send receive error: %w", err)
+		}
+		log.Debugf("Raw AT response: %q, %v", string(response), response)
 
-    	// Read back response and check for OK or ERROR
-    	scanner := bufio.NewScanner(bytes.NewReader(response))
-        awake := false
-    	for scanner.Scan() {
-    		line := strings.TrimSpace(scanner.Text())
-    		if line == "O^K" {
-			awake = true
-    		}
-    		if line == "E^RROR" {
-    			return fmt.Errorf("device returned ERROR")
-    		}
-        }
-        
-        if awake {
-            return nil
-        }
+		// Read back response and check for OK or ERROR
+		scanner := bufio.NewScanner(bytes.NewReader(response))
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			if line == "O^K" {
+				return nil
+			} else if line == "E^RROR" {
+				return fmt.Errorf("device returned ERROR")
+			}
+		}
 
-    	if err := scanner.Err(); err != nil {
-    		return fmt.Errorf("scanner error: %w", err)
-    	}
+		if err := scanner.Err(); err != nil {
+			return fmt.Errorf("scanner error: %w", err)
+		}
 
-    	log.Debugf("no valid O^K/E^RROR response received")
+		log.Debugf("no valid O^K/E^RROR response received")
 
-        if attempt > retries {
-            return fmt.Errorf("Failed to wake up serial device after %d attempts!", attempt)
-        }
-    	time.Sleep(100 * time.Millisecond)
-    }
+		if attempt > retries {
+			return fmt.Errorf("Failed to wake up serial device after %d attempts!", attempt)
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func sendATCommand(command string, baudRate int) error {
@@ -217,8 +213,7 @@ func sendATCommand(command string, baudRate int) error {
 		line := strings.TrimSpace(scanner.Text())
 		if line == "O^K" {
 			return nil
-		}
-		if line == "E^RROR" {
+		} else if line == "E^RROR" {
 			return fmt.Errorf("device returned ERROR")
 		}
 	}
